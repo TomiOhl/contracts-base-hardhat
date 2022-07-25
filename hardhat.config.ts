727f547e0e9664fd03ef2f4dd7ef49684fb92ea7
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-07-25 17:54:30 +0200

    Bump Solidity version Using v0.8.9 as that's the latest one fully supported by Hardhat. Probably it would be more useful to bump it to latest to avoid unnecessary vulnerabilities and gain possible optimizations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,7 +19,7 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 // Go to https://hardhat.org/config/ to learn more.
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.4",
+    version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,

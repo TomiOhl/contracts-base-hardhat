261e4a05b078a6defaee3a5e13a118239bd72e1f
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-07-25 18:50:30 +0200

    Update Prettier and Eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,7 @@ module.exports = {
     node: true
   },
   plugins: ["@typescript-eslint"],
-  extends: [
-    "airbnb-base",
-    "prettier",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended"
-  ],
+  extends: ["airbnb-base", "prettier", "plugin:node/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12
@@ -21,9 +16,6 @@ module.exports = {
     "no-console": "off",
     "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
     "node/no-unpublished-import": "off",
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] }
-    ]
+    "node/no-unsupported-features/es-syntax": ["error", { ignores: ["modules"] }]
   }
 };

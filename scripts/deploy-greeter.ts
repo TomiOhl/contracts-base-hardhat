52a141110ad464d03d68634eba78765fcac1f06d
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-10-03 19:37:04 +0200

    More informative deploy script

--- scripts/deploy-greeter.ts
+++ scripts/deploy-greeter.ts
@@ -5,6 +5,9 @@
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
+// Constructor argument
+const greeting = "Hello, Hardhat!";
+
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command line interface.
   //
@@ -14,11 +17,18 @@ async function main() {
 
   // We get the contract to deploy
   const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const greeter = await Greeter.deploy(greeting);
+
+  console.log(
+    `Deploying contract to ${
+      ethers.provider.network.name !== "unknown" ? ethers.provider.network.name : ethers.provider.network.chainId
+    }...`
+  );
 
   await greeter.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
+  console.log("Constructor arguments:", greeting);
 }
 
 // We recommend this pattern to be able to use async/await everywhere and properly handle errors.
